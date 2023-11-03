c87753e7ab6d8903a8efadcb4b96e011f9b19394
oliujunk <413041277@qq.com>
2023-11-03 15:21:41 +0800

    update

--- output.go
+++ output.go
@@ -2,18 +2,7 @@ package main
 
 import (
 	"log"
-	"oliujunk/output/bjhlc"
-	"oliujunk/output/hangjingqi"
-	"oliujunk/output/houtuyun"
-	houtuyun_t "oliujunk/output/houtuyun_test"
-	"oliujunk/output/jiamusi"
-	"oliujunk/output/jiangsushengnywlw"
-	"oliujunk/output/shangma"
-	"oliujunk/output/soilmoisture"
-	"oliujunk/output/taicangnywlw"
-	"oliujunk/output/zhongrui"
-	"oliujunk/output/zhongyaocai"
-	"oliujunk/output/zjtpyun"
+	"oliujunk/output/wssw"
 )
 
 func init() {
@@ -23,31 +12,7 @@ func init() {
 
 func main() {
 
-	soilmoisture.Start()
-
-	jiamusi.Start()
-
-	hangjingqi.Start()
-
-	zhongrui.Start()
-
-	houtuyun.Start()
-
-	houtuyun_t.Start()
-
-	shangma.Start()
-
-	zjtpyun.Start()
-
-	zhongyaocai.Start()
-
-	jiangsushengnywlw.Start()
-
-	taicangnywlw.Start()
-
-	bjhlc.Start()
-
-	//wssw.Start()
+	wssw.Start()
 
 	select {}
 }
